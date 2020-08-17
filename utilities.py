ae09866eac052ce817c569f534d2809fd634a6e9
eshwarkoka <eshwar.koka@gmail.com>
2020-08-17 14:04:22 +0530

    updated utilities.py

--- utilities.py
+++ utilities.py
@@ -1,8 +1,13 @@
+# utilities.py
+# all general utilities or functions are added here
+
+
 import os
 import sys
+import time
 
 
-# check if virtual environment is activated or not
+# checks if virtual environment is activated or not
 def check_venv():
     project_path = os.getcwd()
     venv_path = '/venv/bin/python'
@@ -13,3 +18,23 @@ def check_venv():
         print("** Virtual Environment is not activated **")
         print("""** run "source venv/bin/activate" to activate virtual environment  **""")
         sys.exit()
+
+
+# returns epoch time 
+def get_epoch_time():
+    return int(time.time())
+
+
+# checks if epoch difference is less than 24 hrs or not
+def check_if_epoch_difference_less_than_24hrs(epoch_1, epoch_2):
+    # epoch_1 < epoch_2
+    if (int(epoch_2) - int(epoch_1)) < 86400:
+        return True
+    return False
+
+
+# checks if href in anchor tag is valid or not
+def href_is_valid(href):
+    if href == "" or href == "/" or href == "#" or href == "javascript:;":
+        return False
+    return True
